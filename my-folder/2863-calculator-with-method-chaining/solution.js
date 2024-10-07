4111fdf42e5c7c530102b11189e4ec96148c11c6
klopez5783 <125326432+klopez5783@users.noreply.github.com>
2024-10-07 12:55:22 +0000

    [LeetCode Sync] Runtime - 56 ms (6.52%), Memory - 48.2 MB (100.00%)

---
+++ my-folder/2863-calculator-with-method-chaining/solution.js
@@ -0,0 +1,62 @@
+class Calculator {
+    
+    /** 
+     * @param {number} value
+     */
+    constructor(value) {
+        this.x = value;
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    add(value){
+        this.x += value;
+        return this;
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    subtract(value){
+        this.x -= value;
+        return this;
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */  
+    multiply(value) {
+        this.x *= value;
+        return this;
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    divide(value) {
+        if(value === 0) throw "Division by zero is not allowed";
+        this.x /= value;
+        return this;
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    power(value) {
+        this.x = Math.pow(this.x, value);
+        return this;
+    }
+    
+    /** 
+     * @return {number}
+     */
+    getResult() {
+        return this.x;
+    }
+}
