8a2051b6206490ac91000a3357fa06333ab8885f
klopez5783 <125326432+klopez5783@users.noreply.github.com>
2024-10-09 14:02:06 +0000

    [LeetCode Sync] Runtime - 56 ms (18.01%), Memory - 49.1 MB (100.00%)

---
+++ my-folder/2762-cache-with-time-limit/solution.js
@@ -0,0 +1,54 @@
+var TimeLimitedCache = function() {
+    this.cache = new Map();
+};
+
+/** 
+ * @param {number} key
+ * @param {number} value
+ * @param {number} duration time until expiration in ms
+ * @return {boolean} if un-expired key already existed
+ */
+TimeLimitedCache.prototype.set = function(key, value, duration) {
+    const exists = this.cache.has(key)
+    if(exists){
+        clearTimeout(this.cache.get(key).timeoutID)
+    }
+    const timeoutID = setTimeout(()=>{
+        this.cache.delete(key)//remove key
+    },duration)
+    this.cache.set(key,{
+        value, timeoutID
+    })
+
+    // this.cache.set(key, {
+    //   value,
+    //   value,
+    //   timerId: setTimeout(() => {
+    //     this.cache.delete(key);
+    //   }, duration),
+    // });
+
+    return exists;
+};
+
+/** 
+ * @param {number} key
+ * @return {number} value associated with key
+ */
+TimeLimitedCache.prototype.get = function(key) {
+    return this.cache.has(key) ? this.cache.get(key).value : -1;
+};
+
+/** 
+ * @return {number} count of non-expired keys
+ */
+TimeLimitedCache.prototype.count = function() {
+    return this.cache.size;
+};
+
+/**
+ * const timeLimitedCache = new TimeLimitedCache()
+ * timeLimitedCache.set(1, 42, 1000); // false
+ * timeLimitedCache.get(1) // 42
+ * timeLimitedCache.count() // 1
+ */
