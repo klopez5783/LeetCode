b6cb8bc3fb0221913c5d5e3cf4a84d65f6a3bf46
klopez5783 <125326432+klopez5783@users.noreply.github.com>
2025-06-03 10:47:58 +0000

    [LeetCode Sync] Runtime - 12 ms (3.65%), Memory - 57.2 MB (5.06%)

---
+++ my-folder/0058-length-of-last-word/solution.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+
+    let count = 0;
+    let i = s.length - 1
+
+    while( i > 0 && s[i] == ' ' ){
+        i--
+    }
+
+    while ( i >= 0 && s[i] != " " ){
+        i--
+        count++
+        console.log(`letter : ${s[i]}`)
+    }
+
+    return count
+};
