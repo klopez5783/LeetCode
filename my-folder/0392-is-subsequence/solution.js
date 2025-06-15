a38fa00727786d6ca32222ef2beea96e013bf865
klopez5783 <125326432+klopez5783@users.noreply.github.com>
2025-06-15 18:59:18 +0000

    [LeetCode Sync] Runtime - 1 ms (71.26%), Memory - 55.1 MB (70.85%)

---
+++ my-folder/0392-is-subsequence/solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    let count = 0;
+    for(let i = 0 ; i < t.length ; i++){
+        if(s[count] == t[i] ){
+            count++
+        }
+    }
+
+    return count == s.length;
+    
+};
