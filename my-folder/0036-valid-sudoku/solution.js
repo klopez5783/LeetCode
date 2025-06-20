f8d72882e939bbd7a2db627a5384329b737accae
klopez5783 <125326432+klopez5783@users.noreply.github.com>
2025-06-20 16:13:43 +0000

    [LeetCode Sync] Runtime - 7 ms (33.65%), Memory - 57.7 MB (51.54%)

---
+++ my-folder/0036-valid-sudoku/solution.js
@@ -0,0 +1,29 @@
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+var isValidSudoku = function(board) {
+    
+    const seen = new Set();
+
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      const number = board[i][j];
+      if (number === ".") continue;
+
+      const keyRow = `${number} in row ${i}`;
+      const keyCol = `${number} in column ${j}`;
+      const keyBox = `${number} in submatrix ${Math.floor(i / 3)}-${Math.floor(j / 3)}`;
+
+      if (seen.has(keyRow) || seen.has(keyCol) || seen.has(keyBox)) {
+        return false;                // duplicate detected
+      }
+
+      seen.add(keyRow);
+      seen.add(keyCol);
+      seen.add(keyBox);
+    }
+  }
+
+  return true;                       // no duplicates → valid board
+};
